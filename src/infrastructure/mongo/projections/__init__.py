1a0a996c9c0ce8d13eabd6aceaab10b3575d6820
Dmitry Patin <multik.patin@yandex.ru>
2026-05-06 22:03:33 +0300

    replace projections

---
+++ src/infrastructure/mongo/projections/__init__.py
@@ -0,0 +1,5 @@
+from .base import IDProjection
+from .layout import PaginatedLayoutProjection
+from .report import LayoutIDProjection
+
+__all__ = ["IDProjection", "LayoutIDProjection", "PaginatedLayoutProjection"]
